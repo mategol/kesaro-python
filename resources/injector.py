13a92c0c81ccbfd291c6277e04022e4510fa945b
Mateusz Gołembowski <44233157+mategol@users.noreply.github.com>
2022-06-24 14:33:25 +0200

    prepare file structure

--- resources/injector.py
+++ resources/injector.py
@@ -1,1 +1,12 @@
+import os
+from shutil import rmtree
 
+def inject(file_to_inject, target_file, clone_metadata):
+    if os.path.isdir('cached_files'):
+        rmtree('cached_files')
+    if os.path.isdir('temporary_files'):
+        rmtree('temporary_files')
+    os.mkdir('cached_files')
+    os.mkdir('temporary_files')
+
+    
