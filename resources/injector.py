ec478ab3e8531554834393e2d30846da8191da4e
Mateusz Gołembowski <44233157+mategol@users.noreply.github.com>
2022-06-24 23:14:54 +0200

    Copy input files into cached folder

--- resources/injector.py
+++ resources/injector.py
@@ -1,7 +1,9 @@
 import os
-from shutil import rmtree
+from shutil import rmtree, copy2
+from properties_manager import *
+from file_manager import *
 
-def inject(file_to_inject, target_file, clone_metadata):
+def inject(file_to_inject, target_file, clone_properties):
     if os.path.isdir('cached_files'):
         rmtree('cached_files')
     if os.path.isdir('temporary_files'):
@@ -9,4 +11,15 @@ def inject(file_to_inject, target_file, clone_metadata):
     os.mkdir('cached_files')
     os.mkdir('temporary_files')
 
-    
+    if clone_properties:
+        clone_file_properties(target_file)
+
+    if os.path.isfile(file_to_inject) and os.path.isfile(target_file):
+        copy2(file_to_inject, 'cached_files/inject.exe')
+        copy2(file_to_inject, 'cached_files/target.exe')
+
+
+
+
+    else:
+        print('asd')
