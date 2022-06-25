d79f754401abf5b9954fee0395fca5cebc9c1e46
Mateusz Gołembowski <44233157+mategol@users.noreply.github.com>
2022-06-25 11:15:09 +0200

    Created manual path selection function

--- resources/file_manager.py
+++ resources/file_manager.py
@@ -1,4 +1,5 @@
 from shutil import copy2
+from tkinter import Tk, filedialog
 
 def generate_file_loader(mode):
     if mode == '==':
@@ -6,9 +7,17 @@ def generate_file_loader(mode):
 
     with open('temporary_files/file_loader.py', 'r') as file_loader:
         file_content = file_loader.readlines()
-        file_content[0] = "injected_program_path='cachedfiles/inject.exe'\n"
-        file_content[1] = "original_program_path='cachedfiles/target.exe'\n"
+        file_content[0] = "injected_program_path='cached_files/inject.exe'\n"
+        file_content[1] = "original_program_path='cached_files/target.exe'\n"
 
     with open('temporary_files/file_loader.py', 'w') as file_loader:
         for i in file_content:
             file_loader.write(i)
+
+def get_manual_path(message, extension):
+    root = Tk()
+    root.withdraw()
+    root.attributes('-topmost', True)
+    open_dir = filedialog.askopenfilename(filetypes=[(message, extension)])
+    root.destroy()
+    return open_dir
