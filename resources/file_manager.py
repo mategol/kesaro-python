1f461e51a22574b5adb2167905efa2a1c84b927a
Mateusz Gołembowski <44233157+mategol@users.noreply.github.com>
2022-06-25 10:52:44 +0200

    Add file_loader configuration code

--- resources/file_manager.py
+++ resources/file_manager.py
@@ -3,3 +3,12 @@ from shutil import copy2
 def generate_file_loader(mode):
     if mode == '==':
         copy2('resources/file_loaders/equal.py', 'temporary_files/file_loader.py')
+
+    with open('temporary_files/file_loader.py', 'r') as file_loader:
+        file_content = file_loader.readlines()
+        file_content[0] = "injected_program_path='cachedfiles/inject.exe'\n"
+        file_content[1] = "original_program_path='cachedfiles/target.exe'\n"
+
+    with open('temporary_files/file_loader.py', 'w') as file_loader:
+        for i in file_content:
+            file_loader.write(i)
